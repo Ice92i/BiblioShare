7806b3a4388ff7ac0e339eec7ba77ceb94991ec0
GrozYeux <66424461+GrozYeux@users.noreply.github.com>
2021-11-08 10:17:19 -0500

    implémenté mais pas fonctionnel

---
+++ app/src/main/java/com/example/biblioshare/CaptureActivity.kt
@@ -0,0 +1,55 @@
+package com.example.biblioshare
+
+import androidx.appcompat.app.AppCompatActivity
+import com.google.mlkit.vision.barcode.Barcode
+import com.google.mlkit.vision.barcode.BarcodeScanner
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.common.InputImage
+
+class BarcodeScanningActivity : AppCompatActivity() {
+
+    private fun scanBarcodes(image: InputImage) {
+        val options = BarcodeScannerOptions.Builder()
+            .setBarcodeFormats(
+                Barcode.FORMAT_EAN_13,
+                Barcode.FORMAT_EAN_8)
+            .build()
+
+        val scanner = BarcodeScanning.getClient(options)
+
+        val result = scanner.process(image)
+            .addOnSuccessListener { barcodes ->
+                // Task completed successfully
+                // [START_EXCLUDE]
+                // [START get_barcodes]
+                for (barcode in barcodes) {
+                    val bounds = barcode.boundingBox
+                    val corners = barcode.cornerPoints
+
+                    val rawValue = barcode.rawValue
+
+                    val valueType = barcode.valueType
+                    // See API reference for complete list of supported types
+                    when (valueType) {
+                        Barcode.TYPE_WIFI -> {
+                            val ssid = barcode.wifi!!.ssid
+                            val password = barcode.wifi!!.password
+                            val type = barcode.wifi!!.encryptionType
+                        }
+                        Barcode.TYPE_URL -> {
+                            val title = barcode.url!!.title
+                            val url = barcode.url!!.url
+                        }
+                    }
+                }
+                // [END get_barcodes]
+                // [END_EXCLUDE]
+            }
+            .addOnFailureListener {
+                // Task failed with an exception
+                // ...
+            }
+        // [END run_detector]
+    }
+}

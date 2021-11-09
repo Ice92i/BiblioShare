f0c3474d2e94351dea15fd27126d951325d77dea
GrozYeux <66424461+GrozYeux@users.noreply.github.com>
2021-11-08 22:14:53 -0500

    fin implementation + début string

--- app/src/main/java/com/example/biblioshare/BarcodeScanningActivity.kt
+++ app/src/main/java/com/example/biblioshare/BarcodeScanningActivity.kt
@@ -1,13 +1,50 @@
 package com.example.biblioshare
 
+import android.R.attr
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.media.Image
+import android.net.Uri
+import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.google.mlkit.vision.barcode.Barcode
 import com.google.mlkit.vision.barcode.BarcodeScanner
 import com.google.mlkit.vision.barcode.BarcodeScannerOptions
 import com.google.mlkit.vision.barcode.BarcodeScanning
 import com.google.mlkit.vision.common.InputImage
+import android.R.attr.button
 
 class BarcodeScanningActivity : AppCompatActivity() {
+
+    val barcodeResult = 0
+    var captureButton: Button? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_accueil)
+        captureButton = findViewById(R.id.scan)
+        captureButton?.setOnClickListener { v -> onClickCapture(v) }
+    }
+
+    private var resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val bitmap: Bitmap = MediaStore.Images.Media.getBitmap(contentResolver, result.data!!.data)
+            val inputImage: InputImage = InputImage.fromBitmap(bitmap,0)
+            this.scanBarcodes(inputImage)
+        }
+    }
+
+    fun onClickCapture(view: View?) {
+        val intent: Intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        resultLauncher.launch(intent)
+    }
 
     private fun scanBarcodes(image: InputImage) {
         val options = BarcodeScannerOptions.Builder()

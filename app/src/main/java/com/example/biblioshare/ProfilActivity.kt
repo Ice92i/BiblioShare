f87a8b090fbbc0c6052f8e0e5ec2b305bdfe6eb7
Ice92i <Hayal.issam@gmail.com>
2021-11-28 19:02:01 -0500

    Modifications des donnees + amelioration

--- app/src/main/java/com/example/biblioshare/ProfilActivity.kt
+++ app/src/main/java/com/example/biblioshare/ProfilActivity.kt
@@ -1,10 +1,14 @@
 package com.example.biblioshare
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.auth.ktx.userProfileChangeRequest
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_profil.*
 
@@ -13,10 +17,32 @@ class ProfilActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profil)
 
+        editTextMail.setText(Firebase.auth.currentUser?.email)
+        editTextPseudo.setText(Firebase.auth.currentUser?.displayName)
+
+
         profil_accueil_button.setOnClickListener {
             val intent = Intent(this, AccueilActivity::class.java)
             startActivity(intent)
 
+        }
+        modifier_profile_button.setOnClickListener {
+            val user = Firebase.auth.currentUser
+
+            val profileUpdates =
+                UserProfileChangeRequest.Builder()
+                    .setDisplayName(editTextPseudo.text.toString())
+                    .build()
+            Firebase.auth.currentUser?.updateProfile(profileUpdates)
+            Firebase.auth.currentUser?.updateEmail(editTextMail.text.toString())
+            Firebase.auth.currentUser?.updatePassword(editTextMotDePasse.text.toString())
+
+            user!!.updateProfile(profileUpdates)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Log.d("TAG", "User profile updated.")
+                    }
+                }
         }
         supression_profile_button.setOnClickListener {
             val user = Firebase.auth.currentUser!!
@@ -28,8 +54,8 @@ class ProfilActivity : AppCompatActivity() {
                         val intent = Intent(this, GestionActivity::class.java)
                         startActivity(intent)
                     }
+
                 }
         }
     }
-
 }
